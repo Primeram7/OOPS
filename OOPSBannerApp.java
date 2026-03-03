1c2c7a5bce72e51d832851fef0870241564d52bc
Primeram7 <iit70739@gmail.com>
2026-03-03 08:50:35 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,48 +1,21 @@
-
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-
-        System.out.println(String.join(" ",
-                "  ***   ",
-                "  ***   ",
-                " *****  ",
-                " ***** "));
-
-        System.out.println(String.join(" ",
-                " *   *  ",
-                " *   *  ",
-                " *   *  ",
-                " *     "));
-
-        System.out.println(String.join(" ",
-                "*     * ",
-                "*     * ",
-                " *   *  ",
-                " *     "));
-
-        System.out.println(String.join(" ",
-                "*     * ",
-                "*     * ",
-                " *****  ",
-                " ***** "));
-
-        System.out.println(String.join(" ",
-                "*     * ",
-                "*     * ",
-                " *      ",
-                "     * "));
-
-        System.out.println(String.join(" ",
-                " *   *  ",
-                " *   *  ",
-                " *      ",
-                "     * "));
-
-        System.out.println(String.join(" ",
-                "  ***   ",
-                "  ***   ",
-                " *      ",
-                " ***** "));
+        
+        
+        String[] bannerLines = {
+            String.join("  ", " OOO ", " OOO ", "PPPP ", "SSS  "),
+            String.join("  ", "O   O", "O   O", "P   P", "S    "),
+            String.join("  ", "O   O", "O   O", "P   P", "S    "),
+            String.join("  ", "O   O", "O   O", "PPPP ", " SS  "),
+            String.join("  ", "O   O", "O   O", "P    ", "   S "),
+            String.join("  ", "O   O", "O   O", "P    ", "   S "),
+            String.join("  ", " OOO ", " OOO ", "P    ", "SSS  ")
+        };
+        
+        
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+        
     }
 }
